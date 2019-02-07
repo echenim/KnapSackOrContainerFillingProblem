074de27fa4680b39e5a66af18656fe57cfd8cf6e
Myron William Echenim <myron.echenim@live.com>
2019-02-07 01:21:07 +0100

    implement knapsack method

---
+++ KnapsackProblem/Items.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KnapsackProblem
+{
+    public class Items
+    {
+        public int Weight { get; set; }
+        public int Value { get; set; }
+    }
+}
